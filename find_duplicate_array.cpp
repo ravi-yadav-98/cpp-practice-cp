c62a92016a51ea575dd7c2ceb411f9fca7c9fd3d
Ravi Yadav <ravi.yadav@ubisoft.com>
2022-05-30 18:56:52 +0530

    updated

--- find_duplicate_array.cpp
+++ find_duplicate_array.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 //leetcode problem:
-//Find dupliated element in arry(which is repeating twice)
+//1. Find dupliated element in arry(which is repeating twice)
 //Ex: N=5 [1,2,3,4,3]: --> ans:3
+//brute force approach: Count number of occurance for elements
 
-int findDuplicateElemenet(int arr[], n)
+int findDuplicateElemenet(int arr[], int n)
 {
-
+  int ans =0;
+  for(int i=0;i<n;i++)
+  {
+    ans = ans^arr[i];
+  }
+  for(int i=1;i<n;i++)
+  {
+    ans = ans^i;
+  }
+  return ans;
 }
+
+//2. find all duplicates in array
+// ex: [1,2,3,4,5,3,2,6]--> [2,3]
+//link https://www.youtube.com/watch?v=oVa8DfUDKTw
 int main(void)
 {
     int n;
@@ -19,5 +33,7 @@ int main(void)
     {
     cin>>arr[i];
     }
+    cout<<findDuplicateElemenet(arr, n);
   return 0;
 }
+
